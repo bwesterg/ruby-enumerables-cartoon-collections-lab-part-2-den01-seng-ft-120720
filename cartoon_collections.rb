5ab9d0cdfa06aa6e77bbe237aeabb801a6169644
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-29 01:03:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,20 +25,13 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
   
- # planeteer_calls.find { |element| element = "Earth!" || "Wind!" || #"Fire!" || "Water!" || "Heart!" }
-  
-  #valid_calls.find { |element| element == planeteer_calls.to_s }
-    
-  #planeteer_calls.find do 
-    #|element| element = valid_calls(element) }
-  
  planeteer_calls.find { |element| 
    valid_calls.any? { |call| 
     element == call
     }
  } 
 
-  #planeteer_calls.find { |element| element == valid_calls }
+ 
 end
 
 
